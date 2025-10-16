7bb3c46a9fd938fe5130dbeb7510fc0493bd18c6
renzcainap-jpg <renzcainap@gmail.com>
2025-10-17 02:25:19 +0800

    try

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,8 @@
+@include('header')
+@include('sidebar')
+
+                <main>
+                    <div class="container-fluid px-4">
+                    </div>
+                </main>
+@include('footer')
---
+++ resources/views/sidebar.blade.php
@@ -0,0 +1,38 @@
+            <div id="layoutSidenav_nav">
+                <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+                    <div class="sb-sidenav-menu">
+                        <div class="nav">
+                            <div class="sb-sidenav-menu-heading">Management</div>
+                            <a class="nav-link" href="#">
+                                <div class="sb-nav-link-icon"><i class="fas fa-folder-open"></i></div>
+                                Projects
+                            </a>
+                            <a class="nav-link" href="#">
+                                <div class="sb-nav-link-icon"><i class="fas fa-users"></i></div>
+                                Workers
+                            </a>
+                            <a class="nav-link" href="#">
+                                <div class="sb-nav-link-icon"><i class="fas fa-tasks"></i></div>
+                                Assignments
+                            </a>
+
+                            <div class="sb-sidenav-menu-heading">Planning</div>
+                            <a class="nav-link" href="#">
+                                <div class="sb-nav-link-icon"><i class="fas fa-calendar-alt"></i></div>
+                                Timelines / Schedules
+                            </a>
+
+                            <div class="sb-sidenav-menu-heading">Analytics</div>
+                            <a class="nav-link" href="#">
+                                <div class="sb-nav-link-icon"><i class="fas fa-chart-line"></i></div>
+                                Reports / Progress tracking
+                            </a>
+                        </div>
+                    </div>
+                    <div class="sb-sidenav-footer">
+                        <div class="small">Logged in as:</div>
+                        Start Bootstrap
+                    </div>
+                </nav>
+            </div>
+
